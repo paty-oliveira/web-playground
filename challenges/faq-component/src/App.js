02b166375d26f5cdb0df7088b989e309666ce06a
patricia-oliveira <patriciia.mota@gmail.com>
2023-01-16 18:44:35 +0000

    Add draft version of fqa component

--- challenges/faq-component/src/App.js
+++ challenges/faq-component/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 
+const faqs = [
+  {
+    question: "How many bones does a cat have?",
+    answer: "A cat has 230 bones - 6 more than a human",
+  },
+  {
+    question: "How much do cats sleep?",
+    answer: "The average cat sleeps 12-16 hours per day",
+  },
+  {
+    question: "How long do cats live",
+    answer: "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Frequently Asked Questions</h1>
+      <div className='faq-container'>
+        {
+          faqs.map(faq => {
+            return (
+              <div className='faq-card'>
+                <h3>{faq.question}</h3>
+                <h4>{faq.answer}</h4>
+              </div>
+              )
+            })
+        }
+      </div>
     </div>
   );
 }
