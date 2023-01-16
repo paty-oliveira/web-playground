94e7fa586b0b67f1b25c5f82a7bb3e23c843001c
patricia-oliveira <patriciia.mota@gmail.com>
2023-01-16 18:20:31 +0000

    Add title for FAQ component

--- challenges/faq-component/src/App.test.js
+++ challenges/faq-component/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('The FQA component should have a title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const fqaTitle = screen.getByText("Frequently Asked Questions");
+  expect(fqaTitle).toBeInTheDocument();
 });
