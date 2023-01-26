54b0786143d553b56b54683df02e216cc8f0fedf
patricia-oliveira <patriciia.mota@gmail.com>
2023-01-26 16:59:39 +0000

    Add ability to render question&answers based on open key

--- challenges/faq-component/src/App.js
+++ challenges/faq-component/src/App.js
@@ -6,14 +6,17 @@ const faqs = [
   {
     question: "How many bones does a cat have?",
     answer: "A cat has 230 bones - 6 more than a human",
+    open: true
   },
   {
     question: "How much do cats sleep?",
     answer: "The average cat sleeps 12-16 hours per day",
+    open: true
   },
   {
     question: "How long do cats live?",
     answer: "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    open: true
   },
 ];
 
@@ -25,6 +28,7 @@ function App() {
           <Faq
             question={faq.question}
             answer={faq.answer}
+            isOpen={faq.open}
           />)
       }
     </div>
--- challenges/faq-component/src/faq/Faq.jsx
+++ challenges/faq-component/src/faq/Faq.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Faq.css';
 
-export function Faq({ question, answer }) {
+export function Faq({ question, answer, isOpen }) {
     const [ toggle, setToggle] = useState(false);
 
     const handleButtonToggle = () => {
@@ -9,14 +9,16 @@ export function Faq({ question, answer }) {
     }
 
     return (
-        <div className='faq-container'>
-            <div className='faq-item'>
-                <button onClick={handleButtonToggle}>{'>'}</button>
-                <h3>{question}</h3>
+        isOpen && (
+            <div className='faq-container'>
+                <div className='faq-item'>
+                    <button onClick={handleButtonToggle}>{'>'}</button>
+                    <h3>{question}</h3>
+                </div>
+                <div className='faq-item'>
+                    { toggle && <p>{answer}</p>}
+                </div>
             </div>
-            <div className='faq-item'>
-                { toggle && <p>{answer}</p>}
-            </div>
-        </div>
+        )
     )
 }
