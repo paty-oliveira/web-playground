4b15c59df7f6fa971d15e1819332ff09e8fe8d15
patricia-oliveira <patriciia.mota@gmail.com>
2023-02-16 18:36:18 +0000

    Add ability to sum or substraction to the counter buttons

--- react-challenges/undoable-counter/src/components/Counter.jsx
+++ react-challenges/undoable-counter/src/components/Counter.jsx
@@ -6,6 +6,12 @@ export function Counter() {
 
     const [counter, setCounter] = useState(0);
 
+    const handleClickButton = (event) => {
+        event.preventDefault();
+        const currentNumber = parseInt(event.target.value);
+        setCounter(counter + currentNumber);
+    }
+
     return (
         <div className="counter">
             <div className="action-buttons">
@@ -13,16 +19,16 @@ export function Counter() {
                 <Button>Redo</Button>
             </div>
             <div className="counter-actions">
-                <div className="counter-buttons">
-                    <Button className="btn-secondary">-100</Button>
-                    <Button className="btn-secondary">-10</Button>
-                    <Button className="btn-secondary">-1</Button>
+                <div className="counter-buttons" onClick={handleClickButton}>
+                    <Button className="btn-secondary" value="-100">-100</Button>
+                    <Button className="btn-secondary" value="-10">-10</Button>
+                    <Button className="btn-secondary" value="-1">-1</Button>
                 </div>
                 {counter}
-                <div className="counter-buttons">
-                    <Button className="btn-secondary">+1</Button>
-                    <Button className="btn-secondary">+10</Button>
-                    <Button className="btn-secondary">+100</Button>
+                <div className="counter-buttons" onClick={handleClickButton}>
+                    <Button className="btn-secondary" value="+1">+1</Button>
+                    <Button className="btn-secondary" value="+10">+10</Button>
+                    <Button className="btn-secondary" value="+100">+100</Button>
                 </div>
             </div>
             <div className="counter-history">
