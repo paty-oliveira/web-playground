bffca0139c013ba732b9b0d218477ec71a2e6a9c
patricia-oliveira <patriciia.mota@gmail.com>
2023-02-20 20:29:50 +0000

    Add initial implementation of average daily sales challenge

--- js-challenges/orders-avg/src/index.js
+++ js-challenges/orders-avg/src/index.js
@@ -4,9 +4,28 @@ export default class OrdersAnalyzer {
     }
 
     averageDailySales(productId, orders) {
+        const listOfOrders = orders.orders;
 
-        // TODO: Implement
+        const dailyOrders = {}
 
-        return {};
+        for (let order in listOfOrders) {
+            let creationDate = new Date(listOfOrders[order].creationDate)
+            let day = this.weekdays[creationDate.getDay()]; // improve that part by having a default obj with all the days
+            dailyOrders[day] = 0
+
+            const orderLines = listOfOrders[order].orderLines
+            for (let item in orderLines) {
+                if (orderLines[item].productId === productId)
+                    dailyOrders[day] += orderLines[item].quantity
+            }
+        }
+
+        for (let weekday in this.weekdays){ // this should be removed after
+            if (!dailyOrders.hasOwnProperty(this.weekdays[weekday])){
+                dailyOrders[this.weekdays[weekday]] = 0
+            }
+        }
+
+        return dailyOrders
     }
 }
