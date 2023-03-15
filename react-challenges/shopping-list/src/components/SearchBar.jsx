0e1ef93e49afcbb8bd4fe239b7dc6c6d98b84399
patricia-oliveira <patriciia.mota@gmail.com>
2023-03-15 18:24:34 +0000

    Fix issue on the checkbox state

--- react-challenges/shopping-list/src/components/SearchBar.jsx
+++ react-challenges/shopping-list/src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import {foodApiResponse} from "../fakeServer/getGrocery";
 export function SearchBar() {
 	const [searchItem, setSearchItem] = useState("");
 	const [shoppingList, setShoppingList] = useState([]);
-	const [checked, setChecked] = useState(false);
+	const [checked, setChecked] = useState(new Array(shoppingList.length).fill(false));
 
 	const handleChangeSearchBar = (event) => {
 		event.preventDefault();
@@ -22,8 +22,11 @@ export function SearchBar() {
 		setSearchItem("");
 	}
 
-	const handleCheckBoxChange = () => {
-		setChecked(!checked);
+	const handleCheckBoxChange = (position) => {
+		const updatedState = checked.map((item, index) =>
+			index === position ? !item : item
+		)
+		setChecked(updatedState);
 	}
 
 	return (
@@ -58,15 +61,18 @@ export function SearchBar() {
 						shoppingList
 							.map((item, index) =>
 								<li key={index} className="shopping-items">
-									<input value={item} type="checkbox" checked={checked} onChange={handleCheckBoxChange}/>
-									<label>{item}</label>
+									<input
+										className="item-checkbox"
+										value={item}
+										type="checkbox"
+										checked={checked[index]}
+										onChange={handleCheckBoxChange}
+									/>
+									<label className="item-name">{item}</label>
 								</li>
 							)
 					}
-				{/*	Add underline when the checkbox is active */}
-				{/*	Fix checkbox state issue */}
 				</ul>
-
 			</div>
 		</div>
 	);
