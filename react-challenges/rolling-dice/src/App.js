4182031a13777b53f96380dcdbc12d33c99e360d
patricia-oliveira <patriciia.mota@gmail.com>
2023-03-23 19:17:09 +0000

    Add state for dice number

--- react-challenges/rolling-dice/src/App.js
+++ react-challenges/rolling-dice/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
 function App() {
+
+	const [diceNumber, setDiceNumber] = useState("0");
+
+	const handleDiceNumberChange = (event) => {
+		setDiceNumber(event.target.value);
+	}
+
 	return (
 		<div className="App">
 			<div className="rolling-container">
 				<h2 className="rolling-item">Number of Dice</h2>
-				<input className="rolling-item" type={"number"} min={1} max={99}/>
+				<input
+					className="rolling-item"
+					type={"number"}
+					min={1}
+					max={99}
+					onChange={handleDiceNumberChange}
+				/>
 				<Button className="rolling-item" variant="primary">Roll</Button>
 			</div>
 		</div>
-
 	);
 }
 
