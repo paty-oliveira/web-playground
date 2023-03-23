4321da55965a6e02d7efc0fa0c93cb6ad1d21341
patricia-oliveira <patriciia.mota@gmail.com>
2023-03-23 19:07:38 +0000

    Add Dice number input

--- react-challenges/rolling-dice/src/App.js
+++ react-challenges/rolling-dice/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="rolling-container">
+				<h2 className="rolling-item">Number of Dice</h2>
+				<input className="rolling-item" type={"number"} min={1} max={99}/>
+				<Button className="rolling-item" variant="primary">Roll</Button>
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
