5a425e4acfd361e0cc0f30418e1abe6543f2aafb
patricia-oliveira <patriciia.mota@gmail.com>
2023-05-12 16:44:28 +0100

    Add initial structure of Two-Factor Auth component

--- react-challenges/two-factor-auth/src/App.js
+++ react-challenges/two-factor-auth/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>Two-Factor Authenticator</h2>
+			<div className={"two-factor-container"}>
+				<input aria-label={"input-code-one"}/>
+				<input aria-label={"input-code-two"}/>
+				<input aria-label={"input-code-three"}/>
+				<input aria-label={"input-code-four"}/>
+			</div>
+			<Button variant={"success"} className={"submit-btn"}>Submit</Button>
+		</div>
+	);
 }
 
 export default App;
--- react-challenges/two-factor-auth/src/App.test.js
+++ react-challenges/two-factor-auth/src/App.test.js
@@ -1,3 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe("<App />", () => {
+	it('should render the main elements from the', function () {
+		render(<App />);
+
+		const inputCodeOne = screen.getByLabelText("input-code-one");
+		expect(inputCodeOne).toBeInTheDocument();
+
+		const inputCodeTwo = screen.getByLabelText("input-code-two");
+		expect(inputCodeTwo).toBeInTheDocument();
+
+		const inputCodeThree = screen.getByLabelText("input-code-three");
+		expect(inputCodeThree).toBeInTheDocument();
+
+		const inputCodeFour = screen.getByLabelText("input-code-four");
+		expect(inputCodeFour).toBeInTheDocument();
+
+		const submitButton = screen.getByRole("button", { name: /submit/i });
+		expect(submitButton).toBeInTheDocument();
+	});
+})
