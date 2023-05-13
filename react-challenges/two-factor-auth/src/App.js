ca4d1867d7331f6cb8a9c0ab31bce265d9621526
patricia-oliveira <patriciia.mota@gmail.com>
2023-05-13 19:21:36 +0100

    Add two-factor auth functionality

--- react-challenges/two-factor-auth/src/App.js
+++ react-challenges/two-factor-auth/src/App.js
@@ -1,17 +1,46 @@
 import './App.css';
 import Button from "react-bootstrap/Button";
+import { useRef, useState } from "react";
 
 function App() {
+	const [inputs, setInputs] = useState(["", "", "", ""]);
+	const inputRefs = useRef([]);
+
+	const setInputRefs = (index, ref) => {
+		inputRefs.current[index] = ref;
+	};
+
+	const handleInputChange = (index, e) => {
+		const currentNumber = e.target.value;
+		const newInputs = [...inputs];
+		newInputs[index] = currentNumber;
+		setInputs(newInputs);
+
+		if (currentNumber.length === 1 && index < inputRefs.current.length - 1) {
+			inputRefs.current[index + 1].focus();
+		}
+	};
+
 	return (
 		<div className="App">
 			<h2>Two-Factor Authenticator</h2>
-			<div className={"two-factor-container"}>
-				<input aria-label={"input-code-one"}/>
-				<input aria-label={"input-code-two"}/>
-				<input aria-label={"input-code-three"}/>
-				<input aria-label={"input-code-four"}/>
+			<div className="two-factor-container">
+				{inputs.map((value, index) => (
+					<input
+						key={index}
+						ref={(ref) => setInputRefs(index, ref)}
+						aria-label={`input-code-${index}`}
+						type="number"
+						min="0"
+						max="9"
+						value={value}
+						onChange={(e) => handleInputChange(index, e)}
+					/>
+				))}
 			</div>
-			<Button variant={"success"} className={"submit-btn"}>Submit</Button>
+			<Button variant="success" className="submit-btn">
+				Submit
+			</Button>
 		</div>
 	);
 }
--- react-challenges/two-factor-auth/src/App.test.js
+++ react-challenges/two-factor-auth/src/App.test.js
@@ -1,23 +1,65 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 describe("<App />", () => {
-	it('should render the main elements from the', function () {
+	it('should render the main elements from the App', function () {
 		render(<App />);
 
-		const inputCodeOne = screen.getByLabelText("input-code-one");
+		const inputCodeOne = screen.getByLabelText("input-code-0");
 		expect(inputCodeOne).toBeInTheDocument();
 
-		const inputCodeTwo = screen.getByLabelText("input-code-two");
+		const inputCodeTwo = screen.getByLabelText("input-code-1");
 		expect(inputCodeTwo).toBeInTheDocument();
 
-		const inputCodeThree = screen.getByLabelText("input-code-three");
+		const inputCodeThree = screen.getByLabelText("input-code-2");
 		expect(inputCodeThree).toBeInTheDocument();
 
-		const inputCodeFour = screen.getByLabelText("input-code-four");
+		const inputCodeFour = screen.getByLabelText("input-code-3");
 		expect(inputCodeFour).toBeInTheDocument();
 
 		const submitButton = screen.getByRole("button", { name: /submit/i });
 		expect(submitButton).toBeInTheDocument();
+	});
+
+	it('The user should be able only to type digits in the input boxes', async function () {
+		render(<App/>);
+
+		const inputCodeOne = screen.getByLabelText("input-code-0");
+		await act(() => userEvent.type(inputCodeOne, "1"));
+		expect(inputCodeOne.value).toBe("1");
+
+		const inputCodeTwo = screen.getByLabelText("input-code-1");
+		await act(() => userEvent.type(inputCodeTwo, "0"));
+		expect(inputCodeTwo.value).toBe("0")
+
+		const inputCodeThree = screen.getByLabelText("input-code-2");
+		await act(() => userEvent.type(inputCodeThree, "0"));
+		expect(inputCodeThree.value).toBe("0");
+
+		const inputCodeFour = screen.getByLabelText("input-code-3");
+		await act(() => userEvent.type(inputCodeFour, "9"));
+		expect(inputCodeFour.value).toBe("9")
+	});
+
+	it('When the user enters a number in one box, the cursor should advance to the next field, except the last box', async function () {
+		render(<App />);
+
+		const inputCodeOne = screen.getByLabelText("input-code-0");
+		const inputCodeTwo = screen.getByLabelText("input-code-1");
+		const inputCodeThree = screen.getByLabelText("input-code-2");
+		const inputCodeFour = screen.getByLabelText("input-code-3");
+
+		await act(() => userEvent.type(inputCodeOne, "0"));
+		expect(inputCodeTwo).toHaveFocus();
+
+		await act(() => userEvent.type(inputCodeTwo, "1"));
+		expect(inputCodeThree).toHaveFocus();
+
+		await act(() => userEvent.type(inputCodeThree, "9"));
+		expect(inputCodeFour).toHaveFocus();
+
+		await act(() => userEvent.type(inputCodeFour, "4"));
+		expect(inputCodeFour).toHaveFocus();
 	});
 })
