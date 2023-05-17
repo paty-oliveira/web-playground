77c515a97db4b6d501d47af0aef71b04fcbc7b14
patricia-oliveira <patriciia.mota@gmail.com>
2023-05-17 18:37:55 +0100

    Add initial implementation for Image Carousel with mock data

--- react-challenges/image-carousel/src/App.js
+++ react-challenges/image-carousel/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
+
+    const [index, setIndex] = useState(0);
+    const images = ["https://i.redd.it/sg3q5cuedod31.jpg", "https://i.redd.it/naba9ilyvgj81.jpg", "https://i.redd.it/1ubakwq1jr741.jpg"]
+    // mock images
+
+    function handleNextClick() {
+        const newIndex = index + 1;
+        setIndex(newIndex >= images.length ? index: newIndex);
+    }
+
+    function handlePreviousClick() {
+        const newIndex = index - 1;
+        setIndex(newIndex < 0 ? index: newIndex);
+    }
+
+    return (
     <div className="App">
+        <h2>Most Popular Images from Reddit</h2>
+        <div className="carousel-images">
+            <div className={"carousel-buttons"}>
+                <button onClick={handlePreviousClick}>Previous</button>
+                <button onClick={handleNextClick}>Next</button>
+            </div>
+            <img src={images[index]} alt={"most-popular-reddit-images"} aria-label={"images-reddit"}/>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
--- react-challenges/image-carousel/src/App.test.js
+++ react-challenges/image-carousel/src/App.test.js
@@ -1,5 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-});
+describe("<App />", () => {
+	it('should render the main elements of the component', function () {
+		render(<App />);
+
+		const header = screen.getByRole("heading", { name: /most popular images from reddit/i })
+		expect(header).toBeInTheDocument();
+
+		const previousButton = screen.getByRole("button", { name: /previous/i});
+		expect(previousButton).toBeInTheDocument();
+
+		const nextButton = screen.getByRole("button", { name: /next/i });
+		expect(nextButton).toBeInTheDocument();
+
+		const redditImages = screen.getByLabelText("images-reddit");
+		expect(redditImages).toBeInTheDocument();
+	});
+})
