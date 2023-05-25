8d5b8e215b942f433df09e6fd28b4bb3d2cbbc45
patricia-oliveira <patriciia.mota@gmail.com>
2023-05-25 18:23:51 +0100

    Add initial stucture for Histogram

--- react-challenges/data-viz/src/App.js
+++ react-challenges/data-viz/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {dataPoints} from "./mocks/mockDataPoints";
+import Plot from 'react-plotly.js';
 
 function App() {
+
   return (
     <div className="App">
-
+      <h2>Simple Histogram Showing Number Frequency</h2>
+        <Plot
+            data={[
+                {
+                    x: dataPoints,
+                    type: 'histogram',
+                    opacity: 0.6,
+                    marker: {
+                        color: 'green',
+                    },
+                },
+            ]}
+            layout = { {bargap: 0.06, title: 'Number Frequency'} }
+        />
 
     </div>
   );
