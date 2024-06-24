89b514f2319147b6fc70eb04a98b39ee760b51c8
paty-oliveira <patriciia.mota@gmail.com>
2024-06-24 12:42:00 +0100

    Implement Save and Delete button behavior

--- react-challenges/input-mentions/src/components/TextCard/TextCard.spec.tsx
+++ react-challenges/input-mentions/src/components/TextCard/TextCard.spec.tsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import TextCard from "./TextCard";
 
-describe("TextCard", () => {
+describe("TextCard Component Structure", () => {
   it("should display a title", () => {
     const { getByRole } = render(<TextCard />);
 
@@ -41,5 +41,60 @@ describe("TextCard", () => {
     const cleanButton = getAllByRole("button");
 
     expect(cleanButton[1]).toHaveAttribute("name", "clean");
+  });
+});
+
+describe("TextCard Component Interactions", () => {
+  test("The user must be able to write content on the Text Card", () => {
+    const { getByRole } = render(<TextCard />);
+
+    const textBox = getByRole("textbox");
+
+    fireEvent.change(textBox, {
+      target: { value: "I'm writing random stuff" },
+    });
+
+    waitFor(() =>
+      expect(textBox).toHaveTextContent("I'm writing random stuff"),
+    );
+  });
+
+  test("When the user clicks on Save button, the post must be saved", async () => {
+    const { getAllByRole, getByRole, rerender } = render(<TextCard />);
+
+    const textBox = getByRole("textbox");
+    const saveButton = getAllByRole("button")[0];
+
+    // User writes a post
+    fireEvent.change(textBox, {
+      target: { value: "I'm writing random stuff" },
+    });
+
+    // Then clicks on Save button
+    fireEvent.click(saveButton);
+
+    // The user refresh the page and back again to see if the message stills
+    rerender(<TextCard />);
+
+    await waitFor(() =>
+      expect(textBox).toHaveValue("I'm writing random stuff"),
+    );
+  });
+
+  test("When the user clicks on Clean button, the post content must be cleaned up", async () => {
+    const { getAllByRole, getByRole } = render(<TextCard />);
+
+    const textBox = getByRole("textbox");
+    const cleanButton = getAllByRole("button")[1];
+
+    // User writes a post
+    fireEvent.change(textBox, {
+      target: { value: "I'm writing random stuff" },
+    });
+
+    // Then clicks on Clean button
+    fireEvent.click(cleanButton);
+
+    await waitFor(() => expect(textBox).toHaveValue(""));
   });
 });
--- react-challenges/input-mentions/src/components/TextCard/TextCard.tsx
+++ react-challenges/input-mentions/src/components/TextCard/TextCard.tsx
@@ -1,20 +1,41 @@
 import "./style.css";
 import { FaSave } from "react-icons/fa";
 import { MdOutlineCleaningServices } from "react-icons/md";
+import { ChangeEvent, useState } from "react";
 
 const TextCard = () => {
+  const [post, setPost] = useState<string>(localStorage.getItem("post") || "");
+
+  const handleOnChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setPost(event.target.value);
+  };
+
+  const handleOnClickSave = () => {
+    localStorage.setItem("post", JSON.stringify(post));
+  };
+
+  const handleOnClickDelete = () => {
+    localStorage.removeItem("post");
+    setPost("");
+  };
+
   return (
     <div className="wrapper">
       <p role="title-card" className="title">
         Write your post:
       </p>
       <div className="card">
-        <textarea className="texbox" placeholder="Write here something..." />
+        <textarea
+          className="texbox"
+          placeholder="Write here something..."
+          value={post}
+          onChange={handleOnChange}
+        />
         <div className="wrapper-btn">
-          <button name="save" className="btn">
+          <button name="save" className="btn" onClick={handleOnClickSave}>
             <FaSave size={30} />
           </button>
-          <button name="clean" className="btn">
+          <button name="clean" className="btn" onClick={handleOnClickDelete}>
             <MdOutlineCleaningServices size={30} />
           </button>
         </div>
